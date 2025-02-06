a60cc16e7e200ee7d917f312b41584af12bd7abd
estoqueloja <estoqueloja16863@gmail.com>
2025-02-06 13:56:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.set_page_config(
 st.markdown("""
 <style>
 [data-testid="stAppViewContainer"] {
-    background-color: #b4d6bb; /* Fundo escuro */
+    background-color: transparent; /* Fundo escuro */
 </style>
 """, unsafe_allow_html=True)
 
